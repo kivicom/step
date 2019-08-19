6bbd5c982278399962fd87611b819ba23d73a152
Игорь <igorok007@list.ru>
2019-08-19 10:20:54 +0300

    Check validation to save comment

--- store.php
+++ store.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 if(!empty($_POST['name']) && !empty($_POST['text'])){
     require_once 'db.php';
@@ -10,9 +11,21 @@ if(!empty($_POST['name']) && !empty($_POST['text'])){
 
     $query = "INSERT INTO `{$table}` (`name`, `text`) VALUES ('{$name}', '{$text}')";
     $query = mysqli_query($link, $query);
+
     if($query){
+        $_SESSION['com_ok'] = 'Комментарий успешно добавлен';
         echo header('Location: http://marlinstep.loc/');
     }else{
         echo 'Сообщение не отправлено!';
+        $_SESSION['com_err'] = 'Ошибка. Комментарий не добавлен';
+        echo header('Location: http://marlinstep.loc/');
     }
+}else{
+    if(empty($_POST['name'])){
+        $_SESSION['name_err'] = 'Ошибка. Поле имя не заполненно';
+    }
+    if(empty($_POST['text'])){
+        $_SESSION['text_err'] = 'Ошибка. Поле комментарий не заполненно';
+    }
+    echo header('Location: http://marlinstep.loc/');
 }
