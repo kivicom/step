95f07adec6f996fd90884d93da030085f5b6be4f
Игорь <igorok007@list.ru>
2019-08-21 01:24:18 +0300

    VALIDATE_EMAIL

--- register.php
+++ register.php
@@ -1,16 +1,33 @@
 <?php
 session_start();
 
-if(!empty($_POST['name']) || !empty($_POST['email']) || !empty($_POST['password']) || !empty($_POST['password_confirmation'])){
 
-    require_once 'db.php';
+
+function checkEmail($email)
+{
+    if(filter_var($email,FILTER_VALIDATE_EMAIL)){
+        return true;
+    }
+    return false;
+}
+
+
+
+if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['password_confirmation'])){
+    if(!checkEmail($_POST['email'])){
+        $_SESSION['email_err'] = 'Неправильный формат E-mail';
+        echo header('Location: http://marlinstep.loc/register.php');
+        exit();
+    }
 
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $password = md5($_POST['password']);
+    $pass = md5($_POST['password']);
     $password_confirmation = md5($_POST['password_confirmation']);
 
-    $query = "INSERT INTO `users` (`name`, `email`, `password`) VALUE ('{$name}', '{$email}', '{$password}')";
+    require_once 'db.php';
+
+    $query = "INSERT INTO `users` (`name`, `email`, `password`) VALUE ('{$name}', '{$email}', '{$pass}')";
     $query = mysqli_query($link, $query);
 
     if($query){
@@ -21,6 +38,7 @@ if(!empty($_POST['name']) || !empty($_POST['email']) || !empty($_POST['password'
         echo header('Location : /register.php');
     }
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -38,96 +56,97 @@ if(!empty($_POST['name']) || !empty($_POST['email']) || !empty($_POST['password'
     <link href="css/app.css" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="index.php">
-                    Project
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<div id="app">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+                Project
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
 
-                    </ul>
+                </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                            <li class="nav-item">
-                                <a class="nav-link" href="login.php">Login</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="register.php">Register</a>
-                            </li>
-                    </ul>
-                </div>
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="register.php">Register</a>
+                    </li>
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
 
-        <main class="py-4">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-8">
-                        <div class="card">
-                            <div class="card-header">Register</div>
+    <main class="py-4">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">Register</div>
 
-                            <div class="card-body">
-                                <form method="POST" action="">
+                        <div class="card-body">
+                            <form method="POST" action="">
 
-                                    <div class="form-group row">
-                                        <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
+                                <div class="form-group row">
+                                    <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
 
-                                        <div class="col-md-6">
-                                            <input id="name" type="text" class="form-control" name="name" autofocus>
-
-                                                <!--span class="invalid-feedback" role="alert">
-                                                    <strong>Ошибка валидации</strong>
-                                                </span-->
-                                        </div>
+                                    <div class="col-md-6">
+                                        <input id="name" type="text" class="form-control" name="name" autofocus>
                                     </div>
+                                </div>
 
-                                    <div class="form-group row">
-                                        <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
+                                <div class="form-group row">
+                                    <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
 
-                                        <div class="col-md-6">
-                                            <input id="email" type="email" class="form-control" name="email" >
-                                        </div>
+                                    <div class="col-md-6">
+                                        <input id="email" type="email" class="form-control" name="email" >
                                     </div>
-
-                                    <div class="form-group row">
-                                        <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
-
-                                        <div class="col-md-6">
-                                            <input id="password" type="password" class="form-control " name="password"  autocomplete="new-password">
+                                    <?php if(isset($_SESSION['email_err'])):?>
+                                        <div class="alert alert-danger" role="alert">
+                                            <?php echo $_SESSION['email_err']; unset($_SESSION['email_err']);?>
                                         </div>
-                                    </div>
+                                    <?php endif;?>
+                                </div>
 
-                                    <div class="form-group row">
-                                        <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirm Password</label>
+                                <div class="form-group row">
+                                    <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
 
-                                        <div class="col-md-6">
-                                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation"  autocomplete="new-password">
-                                        </div>
+                                    <div class="col-md-6">
+                                        <input id="password" type="password" class="form-control " name="password"  autocomplete="new-password">
                                     </div>
+                                </div>
 
-                                    <div class="form-group row mb-0">
-                                        <div class="col-md-6 offset-md-4">
-                                            <button type="submit" class="btn btn-primary">
-                                                Register
-                                            </button>
-                                        </div>
+                                <div class="form-group row">
+                                    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirm Password</label>
+
+                                    <div class="col-md-6">
+                                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation"  autocomplete="new-password">
                                     </div>
-                                </form>
-                            </div>
+                                </div>
+
+                                <div class="form-group row mb-0">
+                                    <div class="col-md-6 offset-md-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            Register
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-        </main>
-    </div>
+        </div>
+    </main>
+</div>
 </body>
 </html>
