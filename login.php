55ae4cfdb5090c690816031681d53de6357fc6f5
Игорь <igorok007@list.ru>
2019-08-21 17:56:56 +0300

    Authorization

--- login.php
+++ login.php
@@ -1,7 +1,141 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Corporation
- * Date: 18.08.2019
- * Time: 18:04
- */
+session_start();
+
+if(!empty($_POST['email']) && !empty($_POST['password'])){
+    require_once 'db.php';
+
+    $query = mysqli_query($link, "SELECT * FROM `users` WHERE `email` = '{$_POST['email']}'");
+    $user = mysqli_fetch_assoc($query);
+
+    if($_POST['email'] === $user['email']){
+
+        if(md5($_POST['password']) === $user['password']){
+            foreach ($user as $key => $item) {
+                $_SESSION['user'][$key] = $item;
+                echo header('Location: http://marlinstep.loc/');
+                exit();
+            }
+        }else{
+            $_SESSION['pass_err'] = 'Пароль не верный';
+            echo header('Location: http://marlinstep.loc/login.php');
+            exit();
+        }
+    }else{
+        $_SESSION['email_err'] = 'Email не найден';
+        echo header('Location: http://marlinstep.loc/login.php');
+        exit();
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Comments</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="css/app.css" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+                Project
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="register.php">Register</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <main class="py-4">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">Login</div>
+
+                        <div class="card-body">
+                            <form method="POST" action="">
+
+                                <div class="form-group row">
+                                    <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
+
+                                    <div class="col-md-6">
+                                        <input id="email" type="email" class="form-control is-invalid " name="email"  autocomplete="email" autofocus >
+                                        <?php if(isset($_SESSION['email_err'])):?>
+                                            <div class="alert alert-danger" role="alert">
+                                                <?php echo $_SESSION['email_err']; unset($_SESSION['email_err']);?>
+                                            </div>
+                                        <?php endif;?>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+
+                                    <div class="col-md-6">
+                                        <input id="password" type="password" class="form-control" name="password"  autocomplete="current-password">
+                                    </div>
+                                    <?php if(isset($_SESSION['pass_err'])):?>
+                                        <div class="alert alert-danger" role="alert">
+                                            <?php echo $_SESSION['pass_err']; unset($_SESSION['pass_err']);?>
+                                        </div>
+                                    <?php endif;?>
+                                </div>
+
+                                <div class="form-group row">
+                                    <div class="col-md-6 offset-md-4">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="remember" id="remember" >
+
+                                            <label class="form-check-label" for="remember">
+                                                Remember Me
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row mb-0">
+                                    <div class="col-md-8 offset-md-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            Login
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</div>
+</body>
+</html>
