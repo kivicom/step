69638ef95b161539d50f45d70586fb416bb9d037
Игорь <igorok007@list.ru>
2019-08-25 00:23:39 +0300

    Change connect DB to PDO

--- db.php
+++ db.php
@@ -1,11 +1,13 @@
 <?php
 
 //Устанавливаем доступы к базе данных:
+$driver = 'mysql'; // тип базы данных, с которой мы будем работать
 $host = 'localhost'; //имя хоста, на локальном компьютере это localhost
-$user = 'root'; //имя пользователя, по умолчанию это root
-$password = ''; //пароль, по умолчанию пустой
+$db_user = 'root'; //имя пользователя, по умолчанию это root
+$db_password = ''; //пароль, по умолчанию пустой
 $db_name = 'marlinstep'; //имя базы данных
+$charset = 'utf8'; // кодировка по умолчанию
+$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
 
-//Соединяемся с базой данных используя наши доступы:
-$link = mysqli_connect($host, $user, $password, $db_name);
-mysqli_query($link, "SET NAMES 'utf8'");
+$dsn = "$driver:host=$host;dbname=$db_name;charset=$charset";
+$pdo = new PDO($dsn, $db_user, $db_password, $options);
