05c07b8ab9508cb428efc25046de0df3c5885600
Игорь <igorok007@list.ru>
2019-08-25 15:05:20 +0300

    Check Session on Auth user and Change query to PDO

--- store.php
+++ store.php
@@ -4,21 +4,22 @@ session_start();
 if(!empty($_POST['name']) && !empty($_POST['text'])){
     require_once 'db.php';
 
-    $table = 'comments'; //задаем имя таблицы в переменной
-
+    $user_id = isset($_SESSION['user']['id']) ? $_SESSION['user']['id'] : '0';
     $name = $_POST['name'];
     $text = $_POST['text'];
 
-    $query = "INSERT INTO `{$table}` (`name`, `text`) VALUES ('{$name}', '{$text}')";
-    $query = mysqli_query($link, $query);
+    $query = "INSERT INTO `comments` (`user_id`, `name`, `text`) VALUES (?, ?, ?)";
+    $statement = $pdo->prepare($query);
+    $query = $statement->execute(array($user_id, $name, $text));
+    //print_r($query);die();
 
     if($query){
         $_SESSION['com_ok'] = 'Комментарий успешно добавлен';
-        echo header('Location: http://marlinstep.loc/');
+        echo header('Location:/');
     }else{
         echo 'Сообщение не отправлено!';
         $_SESSION['com_err'] = 'Ошибка. Комментарий не добавлен';
-        echo header('Location: http://marlinstep.loc/');
+        echo header('Location: /');
     }
 }else{
     if(empty($_POST['name'])){
@@ -27,5 +28,5 @@ if(!empty($_POST['name']) && !empty($_POST['text'])){
     if(empty($_POST['text'])){
         $_SESSION['text_err'] = 'Ошибка. Поле комментарий не заполненно';
     }
-    echo header('Location: http://marlinstep.loc/');
+    echo header('Location: /');
 }
