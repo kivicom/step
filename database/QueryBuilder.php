81e51a95935b469ed2e9092b2ee9197a11f097bb
Игорь <igorok007@list.ru>
2019-09-01 14:45:45 +0300

    Querys

--- database/QueryBuilder.php
+++ database/QueryBuilder.php
@@ -37,10 +37,51 @@ class QueryBuilder
 
     public function getEmail($email)
     {
-        $query = "SELECT* FROM `users` WHERE `email` = ? LIMIT 1";
+        $query = "SELECT * FROM `users` WHERE `email` = ?";
         $statement = $this->pdo->prepare($query);
         $statement->execute(array($_POST['email']));
-        $email = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $email = $statement->fetch(PDO::FETCH_ASSOC);
         return $email;
+    }
+
+    public function userUpdate($id, $data, $image = "")
+    {
+        $keys = array_keys($data);
+
+        $string = '';
+        foreach ($keys as $key) {
+            $string .= $key . ' = :' . $key . ', ';
+        }
+        $keys = rtrim($string,', ');
+        $keys .= isset($image) ? (', avatar = :avatar') : '';
+        $data['avatar'] = $image;
+        $data['id'] = $id;
+
+        $query = "UPDATE `users` SET {$keys} WHERE id = :id";
+        $statement = $this->pdo->prepare($query);
+        $result = $statement->execute($data);
+        return $result;
+    }
+
+    public function Register($name, $email,$password){
+        $query = "INSERT INTO `users` (`name`, `email`, `password`) VALUE (?, ?, ?)";
+        $statement = $this->pdo->prepare($query);
+        $result = $statement->execute(array($name, $email, $password));
+        return $result;
+    }
+
+    public function liveComments($user_id, $name, $text)
+    {
+        $query = "INSERT INTO `comments` (`user_id`, `name`, `text`) VALUES (?, ?, ?)";
+        $statement = $this->pdo->prepare($query);
+        $result = $statement->execute(array($user_id, $name, $text));
+        return $result;
+    }
+
+    public function updPassword($id, $password)
+    {
+        $query = "UPDATE `users` SET `password` = ? WHERE `id` = ?";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute(array(md5($password), $id));
     }
 }
